b65f883de7314c621b93ef84b913d7ea81d4b4a3
Michaël BOURMEAU <michael.bourmeau@gmail.com>
2018-09-18 14:08:12 +0200

    fichier fait

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -10,10 +10,19 @@
  *
  */
 
+function jadenCase(str) {
+    return str.replace(/\w\S*/g, function(str){
+        return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+    });
+}
 
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.fail('You must write your own tests')
+assert.strictEqual(typeof jadenCase, "function")
+assert.strictEqual(jadenCase('str str str'), 'Str Str Str')
+assert.strictEqual(jadenCase('qsd qsd qsd'), 'Qsd Qsd Qsd')
+assert.strictEqual(jadenCase('str'), 'Str')
+assert.strictEqual(jadenCase('zapzap zap'), 'Zapzap Zap')
 // End of tests */
