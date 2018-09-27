119f7f73b0d1db672f50001fbeebc5b5fcf0235d
Michaël BOURMEAU <michael.bourmeau@gmail.com>
2018-09-27 15:06:16 +0200

    fait

--- exercises/multiply-recursive.js
+++ exercises/multiply-recursive.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*
  * Create a `multiply` function that takes two number as arguments
@@ -11,21 +11,32 @@
 
 // Your code :
 
-//* Begin of tests
-const assert = require('assert')
+const multiply = (number1, number2, tour = "") => {
+  if (number1 === 0 || number2 === 0) {
+    return 0;
+  } else if (number2 < 0) {
+    return -number1 + multiply(number1, number2 + 1)
+  } else {
+    return number1 + multiply(number1, number2 - 1)
+  }
+}
 
-assert.strictEqual(typeof multiply, 'function')
-assert.strictEqual(multiply.length, 2)
-assert.strictEqual(multiply.toString().includes('Math.imul'), false)
-assert.strictEqual(multiply.toString().includes('while'), false)
-assert.strictEqual(multiply.toString().includes('for'), false)
-assert.strictEqual(multiply.toString().includes('*'), false)
-assert.strictEqual(multiply.toString().includes('/'), false)
-assert.strictEqual(multiply(34, 78), 2652)
-assert.strictEqual(multiply(123, 0), 0)
-assert.strictEqual(multiply(0, -230), 0)
-assert.strictEqual(multiply(0, 0), 0)
-assert.strictEqual(multiply(123, -22), -2706)
-assert.strictEqual(multiply(-22, 123), -2706)
-assert.strictEqual(multiply(-22, -123), 2706)
+
+//* Begin of tests
+const assert = require("assert");
+
+assert.strictEqual(typeof multiply, "function");
+assert.strictEqual(multiply.length, 2);
+assert.strictEqual(multiply.toString().includes("Math.imul"), false);
+assert.strictEqual(multiply.toString().includes("while"), false);
+assert.strictEqual(multiply.toString().includes("for"), false);
+assert.strictEqual(multiply.toString().includes("*"), false);
+assert.strictEqual(multiply.toString().includes("/"), false);
+assert.strictEqual(multiply(34, 78), 2652);
+assert.strictEqual(multiply(123, 0), 0);
+assert.strictEqual(multiply(0, -230), 0);
+assert.strictEqual(multiply(0, 0), 0);
+assert.strictEqual(multiply(123, -22), -2706);
+assert.strictEqual(multiply(-22, 123), -2706);
+assert.strictEqual(multiply(-22, -123), 2706);
 // End of tests */
